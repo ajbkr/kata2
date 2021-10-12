acd600385091dd0362d813bfef7f0ad1f9c285a8
Andrew J. Baker <andrew_j_baker2@hotmail.com>
2021-10-12 10:52:29 +0100

    Ensure all unit tests pass using library funcs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-module.exports = class Rack {
-    balls = []
-
-    add(number) {
-        return null
-    }
+function Rack () {
+  this.balls = []
 }
+
+Rack.prototype.add = function (number) {
+  this.balls.push(number)
+  this.balls.sort((a, b) => a - b)
+
+  return this.balls
+}
+
+module.exports = Rack
--- test/main.test.js
+++ test/main.test.js
@@ -1,42 +1,58 @@
-const Rack = require('../src/index')
-var assert = require('assert')
+/* global describe, it */
+const assert = require('assert')
 
-describe('main.js', function() {
-    it('should return empty array if not balls added', function() {
-      const rack = new Rack
-      assert.deepEqual([], rack.balls)
-    })
-    it('should return a single value', function() {
-      const rack = new Rack
-      rack.add(20)
-      assert.deepEqual([20], rack.balls)
-    })
-    it('should return sorted values when added', function() {
-      const rack = new Rack
-      rack.add(20)
-      rack.add(10)
-      assert.deepEqual([10, 20], rack.balls)
-    })
-    it('should sort all new values', function() {
-      const rack = new Rack
-      assert.deepEqual([], rack.balls)
-      rack.add(20)
-      assert.deepEqual([20], rack.balls)
-      rack.add(10)
-      assert.deepEqual([10, 20], rack.balls)
-      rack.add(30)
-      assert.deepEqual([10, 20, 30], rack.balls)
-    })
-    it('should sort lots of balls', function() {
-      const rack = new Rack
-      rack.add(44)
-      rack.add(21)
-      rack.add(53)
-      rack.add(34)
-      rack.add(24)
-      rack.add(12)
-      rack.add(8)
-      rack.add(21)
-      assert.deepEqual([8, 12, 21,  21,  24,  34,  44,  53], rack.balls)
-    })
-});
+const Rack = require('../src/index')
+
+describe('Rack class', function () {
+  it('should return empty array if not balls added', function () {
+    const rack = new Rack()
+
+    assert.deepEqual(rack.balls, [])
+  })
+
+  it('should return a single value', function () {
+    const rack = new Rack()
+
+    rack.add(20)
+
+    assert.deepEqual(rack.balls, [20])
+  })
+
+  it('should return sorted values when added', function () {
+    const rack = new Rack()
+
+    rack.add(20)
+    rack.add(10)
+
+    assert.deepEqual(rack.balls, [10, 20])
+  })
+
+  it('should sort all new values', function () {
+    const rack = new Rack()
+    assert.deepEqual(rack.balls, [])
+
+    rack.add(20)
+    assert.deepEqual(rack.balls, [20])
+
+    rack.add(10)
+    assert.deepEqual(rack.balls, [10, 20])
+
+    rack.add(30)
+    assert.deepEqual(rack.balls, [10, 20, 30])
+  })
+
+  it('should sort lots of balls', function () {
+    const rack = new Rack()
+
+    rack.add(44)
+    rack.add(21)
+    rack.add(53)
+    rack.add(34)
+    rack.add(24)
+    rack.add(12)
+    rack.add(8)
+    rack.add(21)
+
+    assert.deepEqual(rack.balls, [8, 12, 21, 21, 24, 34, 44, 53])
+  })
+})
